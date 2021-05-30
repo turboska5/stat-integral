89b0cdb87ffb2cd17d1a2afc2c3e889587e3d05c
Andrew Nagel <andrewRnagel@gmail.com>
2021-05-29 20:29:29 -0400

    updates

--- src/com/andrewrnagel/statapp/App.java
+++ src/com/andrewrnagel/statapp/App.java
@@ -7,24 +7,14 @@ public class App {
 
     public static void main(String[] args) {
         if(args.length == 2) {
-            double lowerBound = convertArg(args[0]);
-            double upperBound = convertArg(args[1]);
+            int lowerBound = Integer.parseInt(args[0]);
+            int upperBound = Integer.parseInt(args[1]);
 
-            UnivariateFunction thiFunction = new StatDistributionFunction();
-            double solution = new SimpsonIntegrator(10, 20).integrate(10000, thiFunction, lowerBound, upperBound);
+            UnivariateFunction statDistributionFunctionAsUnivariate = new StatDistributionFunction();
+            double solution = new SimpsonIntegrator(10, 20).integrate(10_000, statDistributionFunctionAsUnivariate, lowerBound, upperBound);
             System.out.println("The Simpson integration from [" + lowerBound + "] to [" + upperBound + "] " +
-                "of function [" + thiFunction.toString() + "] is: [" + solution + "]");
+                "of function [" + statDistributionFunctionAsUnivariate.toString() + "] is: [" + solution + "]");
         }
-    }
-
-    public static Double convertArg(String arg) {
-        if("-inf".equalsIgnoreCase(arg)) {
-            return Double.MIN_VALUE;
-        }
-        if("inf".equalsIgnoreCase(arg) || "+inf".equalsIgnoreCase(arg)) {
-            return Double.MAX_VALUE;
-        }
-        return Double.parseDouble(arg);
     }
 
 }
