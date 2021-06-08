03ac196df97ca7a399ee7df0e41b4545d04c9b45
Andrew Nagel <andrewRnagel@gmail.com>
2021-06-07 22:13:06 -0400

    stub for norminv

--- src/main/java/com/andrewrnagel/statapp/norminv/NormInv.java
+++ src/main/java/com/andrewrnagel/statapp/norminv/NormInv.java
@@ -3,13 +3,19 @@ package com.andrewrnagel.statapp.norminv;
 public class NormInv {
     //Main Method
     public static void main(String[] args) {
-        //TODO: What IS the formula?!
+        if(args.length == 1) {
+            double percentage = 0.998;
+            double numStdDevsFromMean = 3;
+            numStdDevsFromMean = compute(percentage);
+            logResult(percentage, numStdDevsFromMean);
+        }
     }
 
 
 
     //Supporting Methods
     protected static double compute(double percentage) {
+        //TODO: What IS the formula?!
         return 0;
     }
     protected static final void logResult(double percentage, double numStdDevsFromMean) {
